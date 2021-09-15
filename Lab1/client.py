50b0e667bed0675825658cce7ad9bd2ea20ec750
Tempeus <36718822+Tempeus@users.noreply.github.com>
2021-09-15 12:41:22 -0400

    Update client.py

--- Lab1/client.py
+++ Lab1/client.py
@@ -7,8 +7,17 @@ filename = 'test.txt'
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((HOST, PORT))
 print("Connection: OK")
-with open(filename, 'rb') as f:
-    content = f.read()
-    s.send(content)
-    print(content)
+fext = filename.split(".")
+print(fext)
+if fext[1] == "txt":
+    with open(filename, 'rb') as f:
+        content = f.read()
+        s.send(content)
+        print(content)
+elif fext[1] == "jpg":
+    print("placeholder")
+else:
+    print("placeholder")
 print("Request message sent.")
+
+s.close()
