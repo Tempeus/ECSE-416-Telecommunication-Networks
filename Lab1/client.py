f9ac2c0a7bf2310dd985591e610805fafd6d645a
Tempeus <36718822+Tempeus@users.noreply.github.com>
2021-09-15 12:20:51 -0400

    Update client.py

--- Lab1/client.py
+++ Lab1/client.py
@@ -2,7 +2,13 @@ import socket
 
 HOST = 'localhost'
 PORT = 65432
+filename = 'test.txt'
 
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect((HOST, PORT))
-s.sendall(b'Hello WORLD')
+print("Connection: OK")
+with open(filename, 'rb') as f:
+    content = f.read()
+    s.send(content)
+    print(content)
+print("Request message sent.")
