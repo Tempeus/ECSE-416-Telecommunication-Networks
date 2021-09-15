979c1b27e09127bd8d2e85abc9d3ff10c1b53246
Tempeus <36718822+Tempeus@users.noreply.github.com>
2021-09-15 18:58:23 -0400

    Update server.py

--- Lab1/server.py
+++ Lab1/server.py
@@ -2,6 +2,7 @@ import socket
 
 HOST = 'localhost'
 PORT = 65432
+newname = "image%s.png"
 
 #Creating a server socket
 serversocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -16,14 +17,45 @@ clientsocket, address = serversocket.accept()
 with clientsocket:
     print('Connection established with:', address)
     while True:
-        data = clientsocket.recv(1024)
+        data = clientsocket.recv(4096)
         print('Request Message received.')
         if not data:
-          print('Server HTTP Response: HTTP 404 Not Found')
-          break;
+            print('Server HTTP Response: HTTP 404 Not Found')
+            break
         else:
-          #parse the content of the data
-          txt = str(data)
-          print(txt)
-          break;
+            #parse the content of the data
+            txt = str(data)
+            filetype = ''
+            print(txt)
+            
+            #receive message type and send receive confirmation
+            if txt.startswith('TYPE'):
+                tmp = txt.split()
+                filetype = tmp[1]
+                serversocket.send(b"RECV TYPE")
+
+            #receive if it is EOT
+            elif txt.startswith('EOT'):
+                serversocket.close()
+                
+            #read the message    
+            elif data:
+                if filetype == "txt":
+                    print(data)
+
+                elif filetype == "png":
+                    recvimg = open(newname, 'wb')
+                    data = serversocket.recv(4096000)
+                    if not data:
+                        recvimg.close()
+                        print("404?")
+                        break
+                    recvimg.write(data)
+                    recvimg.close()
+
+                    print("Message received.")
+                    serversocket.send(b"RECV MSG")
+                    serversocket.close()
+        break
+
 serversocket.close()
