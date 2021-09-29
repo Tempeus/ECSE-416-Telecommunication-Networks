618f5fb0c2be0143f6e9b2f4f0821297988ddf57
Tempeus <36718822+Tempeus@users.noreply.github.com>
2021-09-29 19:33:20 -0400

    Update client.py

--- Lab1/client.py
+++ Lab1/client.py
@@ -3,11 +3,11 @@ import sys
 import time
 import pickle
 
-#Default case for testing
+# ----------------- Default case for testing -----------------
 HOST = '127.0.0.2'
-PORT = 65432
+PORT = 12345
 timeout = 5
-filename = "text.txt"
+filename = "test.txt"
 
 if(len(sys.argv) == 5):
     HOST = str(sys.argv[1])
@@ -20,32 +20,47 @@ elif(len(sys.argv) == 4):
     PORT = int(sys.argv[2])
     filename = str(sys.argv[3])
 
-#else:
-#    print("Incorrect number of arguments")
-#    sys.exit(1)
+else:
+    print("Incorrect number of arguments")
+    sys.exit(1)
 
+# ----------------- Establishing Connection With Server -----------------
 #Create a client socket to send requests and receives message data
 clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #set the timeout
 clientsocket.settimeout(timeout)
 try:
     clientsocket.connect((HOST, PORT))
+
 except socket.error:
     print("Connection: Not Okay")
     sys.exit(1)
 print("Connection: OK")
 
-request = "GET / HTTP/1.1\r\n Content-Type:text/html\n" + filename
+# ----------------- Formulating and Sending HTTP Request to Server -----------------
+if filename.endswith(".txt"):
+    request = "GET / HTTP/1.1\r\n Content-Type:text/html\n" + filename
+elif filename.endswith(".jpg"):
+    request = "GET / HTTP/1.1\r\n Content-Type:image/jpeg\n" + filename
+else:
+    print("file type not supported")
+    sys.exit(1)
 
 #Send file request message
 clientsocket.send(request.encode())
-print("Request Message Sent")
+print("Request Message Sent.")
 
+# ----------------- Receive and Decode Response from Server -----------------
 #Receive server response and content type
 response = clientsocket.recv(1024)
-print("Server HTTP Response:", response.decode())
+print("Server HTTP Response:", response.decode('utf-8'))
+if("404" in response.decode()):
+	print("404 Not Found")
+	clientsocket.close()
+	sys.exit(1)
+
 contenttype = clientsocket.recv(1024)
-print("Content-Type:", contenttype.decode())
+print("Content-Type:", contenttype.decode('utf-8'))
 
 #Begin receiving the data
 datarecv = []
@@ -63,9 +78,12 @@ filecontent = pickle.loads(b"".join(datarecv))
 print("Time elapsed:", (time.time() - starttime))
 print("Number of packets:", numpack)
 
-if(contenttype == "text/html"):
+if(contenttype.decode() == "text/html"):
+    print("Printing file content:")
     print(filecontent)
-elif(contenttype == "image/jpg"):
+
+elif(contenttype.decode() == "image/jpg"):
+    print("Showing Image:")
     filecontent.show()
 
 clientsocket.close()
